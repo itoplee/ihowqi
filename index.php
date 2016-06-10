54a0a5376ab8f3fdcdbf98c9b1172125daba325c
liuyun <lee@itoplee.com>
2016-06-10 11:52:13 +0800

    change config in index.php

--- index.php
+++ index.php
@@ -14,15 +14,15 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/app/');
 // 定义基础路径
-define('CSS_PATH', '/ihaoqi/public/css');
-define('IMG_PATH', '/ihaoqi/public/imgs');
-define('JS_PATH', '/ihaoqi/public/js');
+define('CSS_PATH', '/ihowqi/public/css');
+define('IMG_PATH', '/ihowqi/public/imgs');
+define('JS_PATH', '/ihowqi/public/js');
 // 定义session
 define('LOGIN_KEY_SESSION', 'userId');
 // 与甲方约定的参数名称
-define('PARAM_KEY', 'u');
+define('PARAM_KEY', 'ihq_u');
 // 会员访问基础链接
-define('LINK_BASE_KEY', 'http://localhost/ihaoqi/index.php/');
+define('LINK_BASE_KEY', 'http://localhost/ihowqi/index.php/');
 // 开启调试模式
 define('APP_DEBUG', false);
 // 加载框架引导文件
